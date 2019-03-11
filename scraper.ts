f11250108af9c8dbd724277a3bdcadede23954c6
MichaelBone <Michael.Bone@senet.com.au>
2019-03-11 14:47:28 +1030

    Changed to construct cells based on a grid of points.

--- scraper.ts
+++ scraper.ts
@@ -119,20 +119,33 @@ function intersectRectangles(rectangle1: Rectangle, rectangle2: Rectangle): Rect
 
 function intersectLines(line1: Line, line2: Line) : Point {
     if ((line1.x1 === line1.x2 && line1.y1 === line1.y2) || (line2.x1 === line2.x2 && line2.y1 === line2.y2))
-        return undefined;
+        return undefined;  // ignore zero length lines
   
     let denominator = (line2.y2 - line2.y1) * (line1.x2 - line1.x1) - (line1.x2 - line1.x1) * (line1.y2 - line1.y1);  
     if (denominator === 0)
+        return undefined;  // ignore parallel lines
+  
+    let distance1 = ((line2.x2 - line2.x1) * (line1.y1 - line2.y1) - (line2.y2 - line2.y1) * (line1.x1 - line2.x1)) / denominator;
+    let distance2 = ((line1.x2 - line1.x1) * (line1.y1 - line2.y1) - (line1.y2 - line1.y1) * (line1.x1 - line2.x1)) / denominator;  
+    if (distance1 < 0 || distance1 > 1 || distance2 < 0 || distance2 > 1)  // check that the intersection is within the line segements
         return undefined;
   
-    let slope1 = ((line2.x2 - line2.x1) * (line1.y1 - line2.y1) - (line2.y2 - line2.y1) * (line1.x1 - line2.x1)) / denominator;
-    let slope2 = ((line1.x2 - line1.x1) * (line1.y1 - line2.y1) - (line1.y2 - line1.y1) * (line1.x1 - line2.x1)) / denominator;  
-    if (slope1 < 0 || slope1 > 1 || slope2 < 0 || slope2 > 1)
-        return undefined;
-  
-    let x = line1.x1 + slope1 * (line1.x2 - line1.x1);
-    let y = line1.y1 + slope1 * (line1.y2 - line1.y1);  
+    let x = line1.x1 + distance1 * (line1.x2 - line1.x1);
+    let y = line1.y1 + distance1 * (line1.y2 - line1.y1);  
     return { x: x, y: y };
+}
+
+// Rotates a rectangle 90 degrees clockwise about the origin.
+
+function rotate90Clockwise(rectangle: Rectangle) {
+    let x = -(rectangle.y + rectangle.height);
+    let y = rectangle.x;
+    let width = rectangle.height;
+    let height = rectangle.width;
+    rectangle.x = x;
+    rectangle.y = y;
+    rectangle.width = width;
+    rectangle.height = height;
 }
 
 // Calculates the fraction of an element that lies within a cell (as a percentage).  For example,
@@ -191,8 +204,6 @@ async function parseCells(page) {
     for (let index = 0; index < operators.fnArray.length; index++) {
         let argsArray = operators.argsArray[index];
 
-        console.log(`${Object.entries(pdfjs.OPS).find(pair => pair[1] === operators.fnArray[index])} ${argsArray}`);
-
         if (operators.fnArray[index] === pdfjs.OPS.restore)
             transform = transformStack.pop();
         else if (operators.fnArray[index] === pdfjs.OPS.save)
@@ -226,13 +237,6 @@ async function parseCells(page) {
         }
     }
 
-for (let line of lines)
-    [ line.x, line.y, line.width, line.height ] = [ line.y, line.x, line.height, line.width ];
-for (let line of lines)
-    line.y = -(line.y + line.height);
-for (let line of lines)
-    console.log(`FillRectangle(e.Graphics, ${line.x}f, ${line.y}f, ${line.width}f, ${line.height}f);`);
-
     // Determine all the horizontal lines and vertical lines that make up the grid.  The following
     // is careful to ignore the short lines and small rectangles that could make up vector images
     // outside of the grid (such as a logo).  Otherwise these short lines would cause problems due
@@ -254,41 +258,65 @@ for (let line of lines)
     let horizontalLineComparer = (a, b) => (a.y > b.y) ? 1 : ((a.y < b.y) ? -1 : 0);
     horizontalLines.sort(horizontalLineComparer);
     
-    // Find all intersection points.
+    // Add the start and end points of all lines.
 
     let points: Point[] = [];
-    for (let line1 of lines) {
-        points.push({ x: line1.x, y: line1.y }, { x: line1.x + line1.width, y: line1.y + line1.height });  // add the start and end points of all lines
-        for (let line2 of lines) {
+
+    for (let line of horizontalLines) {
+        let point = { x: line.x, y: line.y };
+        if (!points.some(otherPoint => (point.x - otherPoint.x) ** 2 + (point.y - otherPoint.y) ** 2 < Tolerance ** 2))
+            points.push(point);
+        point = { x: line.x + line.width, y: line.y };
+        if (!points.some(otherPoint => (point.x - otherPoint.x) ** 2 + (point.y - otherPoint.y) ** 2 < Tolerance ** 2))
+            points.push(point);
+    }
+
+    for (let line of verticalLines) {
+        let point = { x: line.x, y: line.y };
+        if (!points.some(otherPoint => (point.x - otherPoint.x) ** 2 + (point.y - otherPoint.y) ** 2 < Tolerance ** 2))
+            points.push(point);
+        point = { x: line.x, y: line.y + line.height };
+        if (!points.some(otherPoint => (point.x - otherPoint.x) ** 2 + (point.y - otherPoint.y) ** 2 < Tolerance ** 2))
+            points.push(point);
+    }
+        
+    // Find all intersection points.
+
+    for (let verticalLine of verticalLines) {
+        for (let horizontalLine of horizontalLines) {
             let point = intersectLines(
-                { x1: line1.x, y1: line1.y, x2: line1.x + line1.width, y2: line1.y + line1.height },
-                { x1: line2.x, y1: line2.y, x2: line2.x + line2.width, y2: line2.y + line2.height });
-            if (point !== undefined)
+                { x1: horizontalLine.x, y1: horizontalLine.y, x2: horizontalLine.x + horizontalLine.width, y2: horizontalLine.y },
+                { x1: verticalLine.x, y1: verticalLine.y, x2: verticalLine.x, y2: verticalLine.y + verticalLine.height });
+            if (point !== undefined && !points.some(otherPoint => (point.x - otherPoint.x) ** 2 + (point.y - otherPoint.y) ** 2 < Tolerance ** 2))
                 points.push(point);  // add the intersection point
         }
     }
-    
-    // Construct cells based on the grid of lines.
+
+    // Construct cells based on the grid of points.
 
     let cells: Cell[] = [];
 
-    for (let horizontalLineIndex = 0; horizontalLineIndex < horizontalLines.length - 1; horizontalLineIndex++) {
-        for (let verticalLineIndex = 0; verticalLineIndex < verticalLines.length - 1; verticalLineIndex++) {
-            let horizontalLine = horizontalLines[horizontalLineIndex];
-            let nextHorizontalLine = horizontalLines[horizontalLineIndex + 1];
-            let verticalLine = verticalLines[verticalLineIndex];
-            let nextVerticalLine = verticalLines[verticalLineIndex + 1];
-            cells.push({ elements: [], x: verticalLine.x, y: horizontalLine.y, width: nextVerticalLine.x - verticalLine.x, height: nextHorizontalLine.y - horizontalLine.y });
-        }
+    for (let point of points) {
+        // Find the next closest point in the X direction (moving across horizontally with
+        // approximately the same Y co-ordinate).
+
+        let closestRightPoint = points.reduce(((previous, current) => (Math.abs(current.y - point.y) < Tolerance && current.x > point.x && (previous === undefined || (current.x - point.x < previous.x - point.x))) ? current : previous), undefined);
+
+        // Find the next closest point in the Y direction (moving down vertically with
+        // approximately the same X co-ordinate).
+
+        let closestDownPoint = points.reduce(((previous, current) => (Math.abs(current.x - point.x) < Tolerance && current.y > point.y && (previous === undefined || (current.y - point.y < previous.y - point.y))) ? current : previous), undefined);
+
+        // Construct a rectangle from the found points.
+
+        if (closestRightPoint !== undefined && closestDownPoint !== undefined)
+            cells.push({ elements: [], x: point.x, y: point.y, width: closestRightPoint.x - point.x, height: closestDownPoint.y - point.y });
     }
 
     // Sort the cells by approximate Y co-ordinate and then by X co-ordinate.
 
     let cellComparer = (a, b) => (Math.abs(a.y - b.y) < Tolerance) ? ((a.x > b.x) ? 1 : ((a.x < b.x) ? -1 : 0)) : ((a.y > b.y) ? 1 : -1);
     cells.sort(cellComparer);
-
-    for (let cell of cells)
-        console.log(`DrawRectangle(e.Graphics, ${cell.x}f, ${cell.y}f, ${cell.width}f, ${cell.height}f);`);
 
     return cells;
 }
@@ -363,12 +391,16 @@ async function parsePdf(url: string) {
         for (let element of elements)
             element.y = -(element.y + element.height);
 
-        if (page.rotate === 90) {
-            console.log(`Page rotation: ${page.rotate}`);
+        if (page.rotate !== 0)  // degrees
+            console.log(`Page is rotated ${page.rotate}°.`);
+
+        if (page.rotate === 90) {  // degrees
             for (let cell of cells)
-                [ cell.x, cell.y, cell.width, cell.height ] = [ cell.y, cell.x, cell.height, cell.width ];
-            for (let element of elements)
-                [ element.x, element.y, element.width, element.height ] = [ element.y, element.x, element.height, element.width ];
+                rotate90Clockwise(cell);
+            for (let element of elements) {
+                rotate90Clockwise(element);
+                [ element.y, element.width, element.height ] = [ element.y - element.width, element.height, element.width ];  // artificial adjustment (based on experimentation)
+            }
         }
 
         // Sort the elements by approximate Y co-ordinate and then by X co-ordinate.
@@ -422,21 +454,21 @@ async function parsePdf(url: string) {
         // Find the heading cells.
 
         if (applicationNumberHeadingCell === undefined) {
-            applicationNumberHeadingCell = cells.find(cell => cell.elements.some(element => element.text.toLowerCase().replace(/\s/g, "") === "development"));
-            receivedDateHeadingCell = cells.find(cell => cell.elements.some(element => element.text.toLowerCase().replace(/\s/g, "") === "dateof"));
-            addressHeadingCell = cells.find(cell => cell.elements.some(element => element.text.toLowerCase().replace(/\s/g, "") === "propertyaddress"));
-            descriptionHeadingCell = cells.find(cell => cell.elements.some(element => element.text.toLowerCase().replace(/\s/g, "") === "natureofdevelopment"));
+            applicationNumberHeadingCell = cells.find(cell => /^(developmentnumber|developmentno\.|appno)/i.test(cell.elements.map(element => element.text).join("").replace(/\s/g, "")));
+            receivedDateHeadingCell = cells.find(cell => /^(dateofapplication|dateofregistration|dateregistered)/i.test(cell.elements.map(element => element.text).join("").replace(/\s/g, "")));
+            addressHeadingCell = cells.find(cell => /^(propertyaddress|locationofdevelopment)/i.test(cell.elements.map(element => element.text).join("").replace(/\s/g, "")));
+            descriptionHeadingCell = cells.find(cell => /^(natureofdevelopment|descriptionofdev)/i.test(cell.elements.map(element => element.text).join("").replace(/\s/g, "")));
         }
 
         if (applicationNumberHeadingCell === undefined) {
             let elementSummary = elements.map(element => `[${element.text}]`).join("");
-            console.log(`No development applications can be parsed from the current page because the "Development No." column heading was not found.  Elements: ${elementSummary}`);
+            console.log(`No development applications can be parsed from the current page because the "Development Number", Development No." or "App No" column heading was not found.  Elements: ${elementSummary}`);
             continue;
         }
 
         if (addressHeadingCell === undefined) {
             let elementSummary = elements.map(element => `[${element.text}]`).join("");
-            console.log(`No development applications can be parsed from the current page because the "Property Address" column heading was not found.  Elements: ${elementSummary}`);
+            console.log(`No development applications can be parsed from the current page because the "Property Address" or "Location of Development" column heading was not found.  Elements: ${elementSummary}`);
             continue;
         }
 
@@ -454,26 +486,28 @@ async function parsePdf(url: string) {
             if (applicationNumberCell === undefined)
                 continue;
             let applicationNumber = applicationNumberCell.elements.map(element => element.text).join("").trim();
-            if (!/[0-9]+\/[0-9]+\/[0-9]+/.test(applicationNumber))  // an application number must be present, for example, "690/006/15"
+            if (!/[0-9]+\/[0-9]+\/[0-9]+/.test(applicationNumber)) { // an application number must be present, for example, "690/006/15"
+                console.log(`Ignoring "${applicationNumber}" because it is not formatted as an application number.`);
                 continue;
+            }
             console.log(`Found development application ${applicationNumber}.`);
 
             // Construct the address.
 
             if (addressCell === undefined) {
-                console.log("Ignoring the development application because it has no address cell.");
+                console.log(`Ignoring the development application "${applicationNumber}" because it has no address cell.`);
                 continue;
             }
 
-            let address = addressCell.elements.map(element => element.text).join("").replace(/\s\s+/g, " ").trim();
+            let address = addressCell.elements.map(element => element.text).join(" ").replace(/\s\s+/g, " ").trim();
 
             if (address === "") {
-                console.log("Ignoring the development application because it has no address.");
+                console.log(`Ignoring the development application "${applicationNumber}" because it has no address.`);
                 continue;
             } 
 
             if (address === "") {  // an address must be present
-                console.log("Ignoring the development application because the address is blank.");
+                console.log(`Ignoring the development application "${applicationNumber}" because the address is blank.`);
                 continue;
             }
 
@@ -481,13 +515,13 @@ async function parsePdf(url: string) {
 
             let description = "";
             if (descriptionCell !== undefined)
-                description = descriptionCell.elements.map(element => element.text).join("").replace(/\s\s+/g, " ").trim();
+                description = descriptionCell.elements.map(element => element.text).join(" ").replace(/\s\s+/g, " ").trim();
 
             // Construct the received date.
 
             let receivedDate = moment.invalid();
             if (receivedDateCell !== undefined && receivedDateCell.elements.length > 0)
-                receivedDate = moment(receivedDateCell.elements.map(element => element.text).join("").replace(/\s\s+/g, " ").trim(), "D MMMM YYYY", true);
+                receivedDate = moment(receivedDateCell.elements.map(element => element.text).join("").replace(/\s\s+/g, " ").trim(), "D/MM/YYYY", true);
 
             developmentApplications.push({
                 applicationNumber: applicationNumber,
@@ -564,8 +598,7 @@ async function main() {
     if (getRandom(0, 2) === 0)
         selectedPdfUrls.reverse();
 
-    // for (let pdfUrl of selectedPdfUrls) {
-    for (let pdfUrl of [ "https://www.mtr.sa.gov.au/webdata/resources/files/June%202016-1.pdf" ]) {
+    for (let pdfUrl of selectedPdfUrls) {
         console.log(`Parsing document: ${pdfUrl}`);
         let developmentApplications = await parsePdf(pdfUrl);
         console.log(`Parsed ${developmentApplications.length} development ${(developmentApplications.length == 1) ? "application" : "applications"} from document: ${pdfUrl}`);
